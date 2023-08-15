ae96e420fb154d37284d99a0e3fb42620eb5afa2
Mehemmed77 <mehemmedkazimzade54@gmail.com>
2023-08-15 16:31:59 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <h1>Tic-Tac-Toe</h1>
+        <div className="status">Player X's turn</div>
+		<div className="board-container">
+			<div className="board">
+			<div className="cell"></div>
+			<div className="cell"></div>
+			<div className="cell"></div>
+			<div className="cell"></div>
+			<div className="cell"></div>
+			<div className="cell"></div>
+			<div className="cell"></div>
+			<div className="cell"></div>
+			<div className="cell"></div>
+			</div>
+		</div>
+        <button className="restart-button" onclick="restartGame()">Restart</button>
+      </>
   );
 }
 
