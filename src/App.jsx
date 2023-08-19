3e44766a0d4ecd23161c37400966266814a04054
Mehemmed77 <mehemmedkazimzade54@gmail.com>
2023-08-19 23:45:01 +0400

    just adding

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
 import './App.css';
-import Square from './Squares';
 import { v4 } from 'uuid'
 import { useState } from 'react';
 
+function Square(props){
+    return(
+        <>
+            <div className="cell" onClick={props.handleClick}>
+            </div>
+        </>
+    );
+};
+
 function App() {
-	let x = <i class="fa-solid fa-x x-icon"></i>;
-	let o = <i class="fa-regular fa-circle o-icon"></i>;
-
 	const [turn,setTurn] = useState("x");
-
-	const [xStatus,changeX] = useState([]);
-	const [oStatus,changeO] = useState([]);
+	const [squares,setSquares] = useState(Array(9).fill(null));
 
 	const l = [1,2,3,4,5,6,7,8,9]
 
-	const allPossibilities = [
-		[1,2,3],[4,5,6],[7,8,9],
-		[1,4,7],[2,5,8],[3,6,9],
-		[1,5,9],[3,5,7]
-	]
+	const handleClick = (e,id) => {
+		if (!squares.includes(id)){
+			let a = squares;
+			a[id-1] = id;
+			setSquares(a);
+
+			if(turn==="x"){
+				setTurn('o');
+			}
+
+			else{
+				setTurn("x");
+			}
+
+			console.log(turn);
+		};
+	}
 
 	return (
 		<>
@@ -26,8 +41,12 @@ function App() {
 			<div className="status">Player X's turn</div>
 			<div className="board-container">
 				<div className="board">
-					{l.map(e => {
-						return <Square key={v4()} id={e} turn={turn} setTurn={setTurn}></Square>
+					{l.map(id => {
+						return <Square key={v4()} id={id} 
+							handleClick={e => handleClick(e,id)} 
+							setTurn={setTurn} turn={turn} > 
+								
+						</Square>
 					})}
 				</div>
 			</div>
